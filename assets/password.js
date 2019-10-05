69898b79b2aefc8ce6022ef980e3754264dece14
minkyukr <minkya_u@daum.net>
2019-10-04 22:43:21 -0700

    the first prompt is okay

--- assets/password.js
+++ assets/password.js
@@ -38,23 +38,46 @@
     // var numbers = '0123456789';
 	// var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
 	
-var justCheck = alert('Answer yes or no!')	
+// var justCheck = alert('Answer yes or no!')	
 
-var wantUpperCase = prompt('Would you like to add upper case letters to your password?');
-	wantUpperCase = wantUpperCase.toLowerCase();
+// var wantUpperCase = prompt('Would you like to add upper case letters to your password?');
+// 	wantUpperCase = wantUpperCase.toLowerCase();
 
-var wantLowerCase = prompt('Would you like to add lower case letters to your password?');
-	wantLowerCase = wantLowerCase.toLowerCase();
+// var wantLowerCase = prompt('Would you like to add lower case letters to your password?');
+// 	wantLowerCase = wantLowerCase.toLowerCase();
 
-var wantNumbers = prompt('Would you like to add numbers to your password?');
-	wantNumbers = wantNumbers.toLowerCase();
+// var wantNumbers = prompt('Would you like to add numbers to your password?');
+// 	wantNumbers = wantNumbers.toLowerCase();
 
-var wantSymbols = prompt('Would you like to add symbols to your password?');
-	wantSymbols = wantSymbols.toLowerCase();
+// var wantSymbols = prompt('Would you like to add symbols to your password?');
+// 	wantSymbols = wantSymbols.toLowerCase();
 
-if (wantUpperCase === 'yes' && wantLowerCase === 'yes' && wantNumbers === 'yes' && wantSymbols === 'yes') {
-	console.log('you want upper case');
+// if (wantUpperCase === 'yes' && wantLowerCase === 'yes' && wantNumbers === 'yes' && wantSymbols === 'yes') {
+// 	console.log('you want upper case');}
+
+
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+var newPassword
+
+function makePassword(){
+	passLength = prompt('How long does your password needs to be? From 8 to 128');
+	passLength = parseInt(passLength,10);
+		if (passLength < 8 || passLength > 128) {
+			alert('Number should be higher than 7 and lower than 129');
+			return;
+		} 
+	upperCase = alert('Would you like to have upper case letters in your password?');
+		if (upperCase === true) {
+	newPassword = +string(upperCase)
+		
+		}
 }
+
+
+makePassword();
 
 
 
